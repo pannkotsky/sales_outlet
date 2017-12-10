3e2fe6619b7ec010de6e8ca98d9d40a5bfbead23
Valerii Kovalchuk <kovvalole@gmail.com>
2017-12-10 21:43:08 +0200

    Show total quantity

--- documents/views.py
+++ documents/views.py
@@ -19,17 +19,18 @@ class ShipmentsView(StaffuserRequiredMixin, ListView):
         context['site_header'] = _('Sales Outlet')
         context['site_title'] = _('Sales Outlet')
         invoices = self.object_list.all()
-        product_code = self.kwargs.get('product_code')
-        if product_code:
-            product = get_object_or_404(Product, pk=product_code)
-            context['product'] = product
-            invoices = invoices.filter(product=product)
         date_string = self.request.GET.get('date')
         if date_string:
             context['date_string'] = date_string
             date = datetime.datetime.strptime(date_string, '%d.%m.%Y').date()
             context['date'] = date
             invoices = invoices.filter(date=date)
+        product_code = self.kwargs.get('product_code')
+        if product_code:
+            product = get_object_or_404(Product, pk=product_code)
+            context['product'] = product
+            invoices = invoices.filter(product=product)
+            context['total_quantity'] = sum(invoices.values_list('product_quantity', flat=True))
         context['invoices'] = invoices
         total_cost = sum([item.cost() for item in invoices])
         context['total_cost'] = total_cost
