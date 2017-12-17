f103966970d4d350b9d19831317fda6955087b9d
Valerii Kovalchuk <kovvalole@gmail.com>
2017-12-17 16:34:30 +0200

    Change heroku settings

--- project_conf/settings/heroku.py
+++ project_conf/settings/heroku.py
@@ -2,7 +2,13 @@ import dj_database_url
 
 from .base import *
 
+DEBUG = False
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'sales-outlet.herokuapp.com'
+]
